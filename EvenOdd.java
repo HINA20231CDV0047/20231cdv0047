33482344db6728990bf88ee8a427b22b922c7e36
HINA20231CDV0047 <hinamariyam58@gmail.com>
2026-03-04 11:59:20 +0530

    Update EvenOdd.java

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,7 +1,7 @@
 public class EvenOdd {
     public static void main(String[] args) {
 
-        int number = 10;   // You can change this number
+        int number = 10;  
 
         if (number % 2 == 0) {
             System.out.println(number + " is Even");
